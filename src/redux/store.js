afbf61a1a034bcf037dceb19b0d53475e9a8becc
Santhosh M <santhoshm854@gmail.com>
2021-10-20 16:17:50 +0530

    Added logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,15 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers/index";
 import thunk from "redux-thunk";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log("action : ", action);
+  return next(action);
+};
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(...[thunk, loggerMiddleware]))
+);
 
 export default store;
